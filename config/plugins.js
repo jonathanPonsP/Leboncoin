a329a69faee4473fbd767eebbaf2b45d76b14a3c
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2024-09-24 19:09:23 +0200

    modified timeout

--- config/plugins.js
+++ config/plugins.js
@@ -10,8 +10,8 @@ module.exports = ({ env }) => ({
       },
       actionOptions: {
         upload: {},
-        uploadStream: {},
-        delete: {},
+        uploadStream: {timeout: 1000 * 60 * 3,} // = 3 minutes,
+        delete: {},;
       },
     },
   },
